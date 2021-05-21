673c9e55737f5b3f1d06f585e69cda5c2c427f85
Jayachnadra17 <84495991+Jayachnadra17@users.noreply.github.com>
2021-05-21 09:19:32 +0530

    Add files via upload

---
+++ avg&sum.py
@@ -0,0 +1,25 @@
+a=int(input('Enter 1st  number'))
+b=int(input('Enter 2nd number'))
+c=int(input('Enter 3rd number'))
+sum=(a+b+c)
+print('The addition of three numbers is ',sum)
+avg=sum/3
+print('The average of three numbers is ',avg)
+
+#Expected output:
+	
+#Enter 1st  number2
+#Enter 2nd number3
+#Enter 3rd number4
+#The addition of three numbers is  9
+#The average of three numbers is  3.0
+
+#your output  :
+	
+#Enter 1st  number2
+#Enter 2nd number3
+#Enter 3rd number4
+#The addition of three numbers is  9
+#The average of three numbers is  3.0
+
+#[Program finished]
