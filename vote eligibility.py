947502d77f8ddc6afacfd3a34c2669ad2e9e0c29
Jayachnadra17 <84495991+Jayachnadra17@users.noreply.github.com>
2021-05-22 12:37:20 +0530

    Add files via upload

---
+++ vote eligibility.py
@@ -0,0 +1,20 @@
+name=input('Enter person name:')
+age=int(input('Enter person age:'))
+if age >=18:
+	print('Eligible for vote')
+else :
+	print('Not Eligible for vote')
+''' 
+Expected output:
+	Enter person name : ram
+	Enter person age : 24
+	Eligible for vote  '''
+	
+	'''
+	 your output:
+	 	
+	Enter person name:ram
+Enter person age:24
+Eligible for vote
+
+[Program finished]'''
