0d30773492913f0a5559ba32568dbef360a21271
Jayachnadra17 <84495991+Jayachnadra17@users.noreply.github.com>
2021-05-22 14:00:23 +0530

    Add files via upload

---
+++ even or odd.py
@@ -0,0 +1,18 @@
+number=int(input('Enter a number'))
+if number%2==0:
+	print('The number is even')
+else:
+	print('The number is odd')
+'''
+Expected out put :
+	
+	Enter a number 5
+	The nuber is odd
+	
+	Your output :
+		
+	Enter a number5
+The number is odd
+
+[Program finished]
+	'''
